7a2b1576e6f38f763813a170698cfa29a403832c
xiaozhongqing <lxlgtzx@126.com>
2018-06-26 15:51:08 +0800

    0626

---
+++ test/udp/send/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	setvbuf(stdout, 0, _IONBF, 0);
+	fflush(stdout);
+
+	int sock = -1;
+	if((sock = socket(AF_INET, SOCK_DGRAM, 0))
+	{
+		printf("ERROR: socket failed");
+		return -1;
+	}
+	const int opt = 1;
+	int nb = 0;
+	nb = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&opt, sizeof(opt));
+	if(nb == -1)
+	{
+		printf("ERROR: setsockopt failt");
+		return -1;
+	}
+	
+	return 0;
+}
